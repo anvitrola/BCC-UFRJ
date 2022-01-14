1249e6667419009dfbdc29fed1975aac33a93b4f
anvitrola <vitoriavianathiss@outlook.com>
2022-01-13 22:29:30 -0300

    data_structure: array queue implementation

---
+++ estrutura_dados/find-celebrities/find-celeb.py
@@ -0,0 +1,10 @@
+from random import random
+from time import time
+
+def printMatrix(matrix):
+    n = len(matrix)
+    for  i in range(n):
+        print(matrix[i])
+
+def createParty (guests, celebrity):
+    
